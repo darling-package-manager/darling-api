07dda7f715d7ccdb61f8bb8e56a64277ff386cf8
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-04-11 09:53:35 -0400

    Add post-install method

--- src/lib.rs
+++ src/lib.rs
@@ -31,17 +31,41 @@ pub trait PackageManager: Send + Sync {
     /// Note that this ***does not*** affect the cache file. This simply supplies the system package install command.
     ///
     /// # Parameters
+    /// - `context` - The darling context, which provides global immutable information about the program.
     /// - `package` - The name of the package to install.
-    /// - `version` - The version of the package to install. If `None`, the latest version should be installed.
     ///
     /// # Returns
     /// An error if the package could not be installed.
     fn install(&self, context: &Context, package: &InstallationEntry) -> anyhow::Result<()>;
 
+    /// This is run after a single or group of packages are installed. The difference between placing code here and in
+    /// [install] is that when running commands like `load-installed`, which load all installed packages into the
+    /// darling config file, this is only run once after all packages are installed, instead of every time an individual
+    /// package is installed.
+    ///
+    /// This is useful for modules such as the core module which needs to rebuild the source code every time a new
+    /// module is added. With this system, we can just rebuild the source once after all of the modules are added,
+    /// instead of every time each individual module is added.
+    ///
+    /// **Note that this will still be run for individual installations *after* the [install] method**.
+    ///
+    /// This method is optional, and has a default implementation of just `Ok(())`.
+    ///
+    /// # Parameters
+    /// - `context` - The darling context, which provides global immutable information about the program.
+    ///
+    /// # Returns
+    /// An error if anything went wrong in the post-installation process. This is different module-to-module so
+    /// no more information than this can be specified here.
+    fn post_install(&self, _context: &Context) -> anyhow::Result<()> {
+        Ok(())
+    }
+
     /// Uninstalls a package from the system. This does ***not*** affect the cache file, it simply removes the package
     /// from the system itself, and `darling-core` will handle removing the package from the cache file.
     ///
     /// # Parameters
+    /// - `context` - The darling context, which provides global immutable information about the program.
     /// - `package` - The name of the package to remove.
     ///
     /// # Returns
@@ -51,6 +75,9 @@ pub trait PackageManager: Send + Sync {
     /// Returns all *explicitly* installed packages on the system; That is, packages which are not dependencies of
     /// other packages. This **should not** read from a darling file; Instead, darling uses this method to update
     /// the file when running `darling require-all`
+    ///
+    /// # Parameters
+    /// - `context` - The darling context, which provides global immutable information about the program.
     ///
     /// # Returns
     /// The name and version of each installed package. as a `Vec<(name: String, version: String)>`.
